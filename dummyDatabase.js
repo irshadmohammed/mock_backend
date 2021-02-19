fcd15af95ac5af12404e19e6a0493c51d678fa5a
Irshad Mohammed <30858487+irshadmohammed@users.noreply.github.com>
2021-02-19 14:49:56 +0530

    Update dummyDatabase.js

--- dummyDatabase.js
+++ dummyDatabase.js
@@ -1,13 +1,1 @@
-let players = `
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-<soap:Body>
-<ns2:mntOverdraftProtectResponse xmlns:ns2="http://tsys.gpn.com/mntOverDraft/">
-<status>
-<code>999</code>
-<message>Request criteria conflict</message>
-</status>
-</ns2:mntOverdraftProtectResponse>
-</soap:Body>
-</soap:Envelope>
-`
-module.exports = players;
+// dummy response goes here
